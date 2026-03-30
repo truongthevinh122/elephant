bc1321e83722a1cf5d787f144a6703cd661a2c72
nekorona23 <truongthevinh122@gmail.com>
2026-03-31 03:42:56 +0700

    Update build-windows.bat

--- build/build-windows.bat
+++ build/build-windows.bat
@@ -1,24 +1,4 @@
 @echo off
-
-if exist elephant.fda del elephant.fda
-
-REM Paths
-set ZIP7="%ProgramFiles%\7-Zip\7z.exe"
-set WINRAR="%ProgramFiles%\WinRAR\WinRAR.exe"
-
-if exist %ZIP7% (
-    echo Compressing using 7-Zip...
-    %ZIP7% a -mx9 elephant.zip "%~dp0..\plugin\*"
-) else if exist %WINRAR% (
-    echo Compressing using WinRAR...
-    %WINRAR% a -afzip -m5 elephant.zip "%~dp0..\plugin\*"
-) else (
-    echo ERROR: No archiver found (7-Zip or WinRAR).
-    pause
-    exit /b
-)
-
+del elephant.fda
+call "%ProgramFiles%\7-zip\7z.exe" a -mx9 elephant.zip "%~dp0..\plugin\*"
 ren elephant.zip elephant.fda
-
-echo Done!
-pause
